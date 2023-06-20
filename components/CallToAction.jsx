1f6ec439a841f9bf99d65f0757382b7b513383ff
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2023-06-20 19:21:15 +0530

    file edit

--- components/CallToAction.jsx
+++ components/CallToAction.jsx
@@ -6,7 +6,7 @@ const CallToAction = () => {
         <div className="bg-[#D7FFE4] flex justify-center items-center flex-col sm:m-10 m-4 rounded-xl sm:p-12 p-8">
             <p className="text-uppercase sm:text-4xl text-3xl text-center font-semibold">Contact us</p>
             <p className="font-extralight text-center mt-2">Let's talk and find the best solution for you and your company together</p>
-            <button className='bg-green text-white sm:px-20 px-6 py-4 rounded-full mt-6'>Contact us </button>
+            <button className='bg-green text-white sm:px-20 px-6 py-4 rounded-full mt-6'>Make Contact </button>
             <Image src={img} className='sm:absolute   right-[40px] w-[180px]'/>
         </div>
     )
