58510ce700abfdb1ef8c82b305a355aa3cff5965
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2023-06-26 00:48:50 +0530

    done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -16,13 +16,13 @@ const Navbar = () => {
             <div className='flex justify-between px-4 py-3 items-center nav 	 top-0 left-0'>
                 <Link href="/"><Image className='w-[180px]' src={logo} /></Link>
                 <ul className='sm:flex hidden'>
-                    <li ><Link href="/" className='text-[1.15rem] text-green'>Home</Link></li>
-                    <li ><Link href="/company" className='text-[1.15rem] text-green'>Company</Link></li>
-                    <li ><Link href="/academy" className='text-[1.15rem] text-green'>Academy</Link></li>
-                    <li ><Link href="/courses" className='text-[1.15rem] text-green'> Courses</Link></li>
-                    <li ><Link href="/marketplace" className='text-[1.15rem] text-green'>Marketplace</Link></li>
-                    <li ><Link href="/aboutus" className='text-[1.15rem] text-green'>Aboutus</Link></li>
-                    <li ><Link href="/hackathon" className='text-[1.15rem] text-green'>Hackathon</Link></li>
+                    <li ><Link href="/" className='text-[1rem] text-green'>Home</Link></li>
+                    <li ><Link href="/company" className='text-[1rem] text-green'>Company</Link></li>
+                    <li ><Link href="/academy" className='text-[1rem] text-green'>Academy</Link></li>
+                    <li ><Link href="/courses" className='text-[1rem] text-green'> Courses</Link></li>
+                    <li ><Link href="/marketplace" className='text-[1rem] text-green'>Marketplace</Link></li>
+                    <li ><Link href="/aboutus" className='text-[1rem] text-green'>Aboutus</Link></li>
+                    <li ><Link href="/hackathon" className='text-[1rem] text-green'>Hackathon</Link></li>
                 </ul>
                 {open ? <XMarkIcon className='sm:hidden relative w-[35px] z-[2010]' onClick={() => setOpen(false)} /> : <Bars3Icon className='sm:hidden relative w-[35px] z-[100]' onClick={() => setOpen(true)} />}
                 <Link href="https://docs.google.com/forms/d/e/1FAIpQLSeKhKzbkfUpONy24bTzoRY-YELlkcHzMaYCXiI3zNvlZEUosg/viewform" className="  text-white bg-black px-5 py-2 rounded-full md:flex hidden text-lg">Get Started</Link>
